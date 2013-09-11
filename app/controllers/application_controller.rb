23f4cb9a2701d5844e782c325cf9a2b1fe136c41
bhawkeswood <bhawkeswood@gmail.com>
2013-09-11 13:03:23 -0400

    Force sign in before show and edit etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
 private
 
 	def current_user
@@ -11,4 +12,11 @@ private
 
   helper_method :current_user
 
+
+  	def ensure_logged_in
+  		unless current_user
+  			flash[:alert] = "Please log in"
+  			redirect_to new_session_path
+  		end
+  	end
 end
